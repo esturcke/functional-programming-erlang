98812666b81ef5ab98936b88c10a02e37226615c
Erik J. Sturcke <esturcke@cisco.com>
2017-02-24 10:22:11 -0500

    Add bit counters bits/1 and bits2/1

---
+++ bits.erl
@@ -0,0 +1,23 @@
+-module(bits).
+-export([bits/1, bits2/1]).
+
+bits(0) ->
+    0;
+
+bits(X) when X > 0, X rem 2 == 1 ->
+    1 + bits(X div 2);
+
+bits(X) when X > 0 ->
+    bits(X div 2).
+
+bits2(X) when X >= 0 ->
+    bits2(X, 0).
+
+bits2(0, A) ->
+    A;
+
+bits2(X, A) when X rem 2 == 1 ->
+    bits2(X div 2, A + 1);
+
+bits2(X, A) ->
+    bits2(X div 2, A).
