54d10209634b4ebb8b3e7778076f5ea6f2cee23e
Erik J. Sturcke <esturcke@cisco.com>
2017-03-05 16:03:14 -0500

    Sort results

--- index.erl
+++ index.erl
@@ -9,7 +9,7 @@
 -spec create(string()) -> [{string(), [{integer(), integer()}]}].
 create(Name) ->
   Lines = number(get_file_contents(Name)),
-  Words = split(Lines), 
+  Words = group(sort(split(Lines))), 
   Words.
 
 % Zip a list with the index in the for each item in the list
@@ -39,6 +39,14 @@ valid(Word) -> lists:all(fun(Letter) when $a =< Letter, Letter =< $z -> true;
                             (Letter) when $A =< Letter, Letter =< $Z -> true;
                             ($') -> true;
                             (_) -> false end, Word).
+
+% Sort word/line numebr pairs by word
+-spec sort([{string(), integer()}]) -> [{string(), integer()}].
+sort(Words) -> lists:sort(fun({WordA, _}, {WordB, _}) -> WordA =< WordB end, Words).
+
+% Group identical words and form 
+-spec group([{string(), integer()}]) -> [{string(), [{integer(), integer()}]}].
+group(A) -> A.
 
 % Used to read a file into a list of lines.
 % Example files available in:
